LineageOS/android_device_htc_m7-common

m7-common: Introduce m7vzw
Change-Id: Iff184007a7906f3fe842f0038c016974034e9217

rootdir/Android.mk
@@ -24,14 +24,14 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/remount.qcom.gsm
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+endif
 
-else  # Non-GSM variants
-
+ifneq ($(filter m7spr,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.qcom
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/fstab.qcom.cdma
+LOCAL_SRC_FILES		:= etc/fstab.qcom.spr
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -47,7 +47,33 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= remount.qcom
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/remount.qcom.cdma
+LOCAL_SRC_FILES		:= etc/remount.qcom.spr
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+endif
+
+ifneq ($(filter m7vzw,$(TARGET_DEVICE)),)
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.qcom
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/fstab.qcom.vzw
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.target.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.target.rc.cdma
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= remount.qcom
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/remount.qcom.vzw
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 endif

SlimRoms/device_htc_m7-common

rootdir/Android.mk
@@ -25,7 +25,8 @@ LOCAL_SRC_FILES		:= etc/remount.qcom.gsm
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-else  # Non-GSM variants
+endif
+ifneq ($(filter m7spr,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.qcom
@@ -48,6 +49,32 @@ LOCAL_MODULE		:= remount.qcom
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/remount.qcom.cdma
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+else  # Verizon variant
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.qcom
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/fstab.qcom.gsm
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.target.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.target.rc.vzw
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= remount.qcom
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/remount.qcom.gsm
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 endif