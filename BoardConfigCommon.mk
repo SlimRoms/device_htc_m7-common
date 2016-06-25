LineageOS/android_device_htc_m7-common

m7-common: Organize boardconfig
Change-Id: I0c2b59e7590595c782d7648ab81dbdf465267ade

BoardConfigCommon.mk
@@ -1,3 +1,4 @@
+# Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,26 +30,13 @@
 
 LOCAL_PATH := device/htc/m7-common
 
-TARGET_SPECIFIC_HEADER_PATH += device/htc/m7-common/include
-
-USE_CLANG_PLATFORM_BUILD := true
-
-# Kernel
-BOARD_KERNEL_BASE := 0x80600000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
-TARGET_KERNEL_CONFIG := m7_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
-TARGET_KERNEL_SOURCE := kernel/htc/msm8960
-
 # Audio
-BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
-BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recognition use case
-BOARD_USES_SEPERATED_FM := true # use distinct fm use cases
-BOARD_USES_SEPERATED_VOICE_SPEAKER := true  # use distinct voice speaker use case
-BOARD_USES_SEPERATED_VOIP := true  # use distinct VOIP use cases
 BOARD_HAVE_HTC_CSDCLIENT := true
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+BOARD_USES_SEPERATED_FM := true
+BOARD_USES_SEPERATED_VOICE_SPEAKER := true
+BOARD_USES_SEPERATED_VOIP := true
 
 # Bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
@@ -66,14 +54,27 @@ BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
 # CMHW
 BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
 
+# General compilation flags
+TARGET_SPECIFIC_HEADER_PATH += device/htc/m7-common/include
+USE_CLANG_PLATFORM_BUILD := true
+
 # Filesystem
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
 BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
 
 # Graphics
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 HAVE_ADRENO_SOURCE := false
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80600000
+BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
+TARGET_KERNEL_CONFIG := m7_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # Recovery
 BOARD_RECOVERY_BLDRMSG_OFFSET := 2048
@@ -90,20 +91,19 @@ BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
-
 BOARD_SEPOLICY_DIRS += device/htc/m7-common/sepolicy
 
 # Wifi
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE                := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcm4335_b0.bin"
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcm4335_apsta_b0.bin"
 WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/firmware/fw_bcm4335_p2p_b0.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcm4335_b0.bin"
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
 
 # inherit from the proprietary version
 -include vendor/htc/m7-common/BoardConfigVendor.mk

SlimRoms/device_htc_m7-common

BoardConfigCommon.mk
@@ -1,3 +1,4 @@
+# Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,26 +30,13 @@
 
 LOCAL_PATH := device/htc/m7-common
 
-TARGET_SPECIFIC_HEADER_PATH += device/htc/m7-common/include
-
-USE_CLANG_PLATFORM_BUILD := true
-
-# Kernel
-BOARD_KERNEL_BASE := 0x80600000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
-TARGET_KERNEL_CONFIG := slim_m7_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
-TARGET_KERNEL_SOURCE := kernel/htc/msm8960
-
 # Audio
-BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
-BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recognition use case
-BOARD_USES_SEPERATED_FM := true # use distinct fm use cases
-BOARD_USES_SEPERATED_VOICE_SPEAKER := true  # use distinct voice speaker use case
-BOARD_USES_SEPERATED_VOIP := true  # use distinct VOIP use cases
 BOARD_HAVE_HTC_CSDCLIENT := true
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+BOARD_USES_SEPERATED_FM := true
+BOARD_USES_SEPERATED_VOICE_SPEAKER := true
+BOARD_USES_SEPERATED_VOIP := true
 
 # Bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
@@ -68,12 +56,25 @@ BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
 
 # Filesystem
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
 BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
+
+# General compilation flags
+TARGET_SPECIFIC_HEADER_PATH += device/htc/m7-common/include
+USE_CLANG_PLATFORM_BUILD := true
 
 # Graphics
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 HAVE_ADRENO_SOURCE := false
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80600000
+BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
+TARGET_KERNEL_CONFIG := slim_m7_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # Recovery
 BOARD_RECOVERY_BLDRMSG_OFFSET := 2048
@@ -94,16 +95,16 @@ BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 BOARD_SEPOLICY_DIRS += device/htc/m7-common/sepolicy
 
 # Wifi
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE                := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcm4335_b0.bin"
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcm4335_apsta_b0.bin"
 WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/firmware/fw_bcm4335_p2p_b0.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcm4335_b0.bin"
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
 
 # inherit from the proprietary version
 -include vendor/htc/m7-common/BoardConfigVendor.mk